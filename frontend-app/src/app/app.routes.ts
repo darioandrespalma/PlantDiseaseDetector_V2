a1ec6764a21a26c19100e0762a6106e961662f7d
Darwin Guachamín <darwinmario98_@hotmail.com>
2026-01-14 09:44:58 -0500

    feat: tareas con calendario lunar y recomendaciones con filtros

--- frontend-app/src/app/app.routes.ts
+++ frontend-app/src/app/app.routes.ts
@@ -1,23 +1,29 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './guards/auth-guard';
 
 import { LoginComponent } from './components/login/login';
 import { RegisterComponent } from './components/register/register';
 import { MainLayoutComponent } from './components/main-layout/main-layout';
 import { DashboardComponent } from './components/dashboard/dashboard';
 import { FincaComponent } from './components/finca/finca';
-import { TareasComponent } from './components/tareas/tareas';
 import { DetectionComponent } from './components/detection/detection';
 import { ResultComponent } from './components/result/result';
-import { RecomendacionComponent } from './components/recomendacion/recomendacion';
 import { MapSelectorComponent } from './components/map-selector/map-selector';
 import { BibliotecaComponent } from './components/biblioteca/biblioteca';
+
+import { TareasComponent } from './pages/tareas/tareas';
+import { RecomendacionesComponent } from './pages/recomendaciones/recomendaciones';
+
+
+import { authGuard } from './guards/auth-guard';
 
 
 export const routes: Routes = [
   // RUTAS PÚBLICAS (Sin Guard)
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'tareas', component: TareasComponent },
+  { path: 'recomendaciones', component: RecomendacionesComponent },
+
 
   // RUTAS PRIVADAS (Protegidas por authGuard)
   {
@@ -27,18 +33,14 @@ export const routes: Routes = [
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'finca', component: FincaComponent },
-      { path: 'tareas', component: TareasComponent },
+      //{ path: 'tareas', component: TareasComponent },
+      //{ path: 'recomendaciones', component: RecomendacionesComponent },
       { path: 'deteccion/:crop', component: DetectionComponent },
       { path: 'result/:id', component: ResultComponent },
-
-      // ya tenías:
-      { path: 'recomendaciones', component: RecomendacionComponent },
-
-      // ✨ NUEVA RUTA DEL MAPA
       { path: 'mapa', component: MapSelectorComponent },
       { path: 'biblioteca', component: BibliotecaComponent },
 
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     ]
   },
 
